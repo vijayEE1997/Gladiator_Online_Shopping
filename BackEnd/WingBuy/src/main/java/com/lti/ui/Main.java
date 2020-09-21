b00ad17b6b9a50c024144ce1d886ad2ef3b17569
unknown <ankamgnapika@gmail.com>
2020-09-22 00:26:01 +0530

    csv file added

--- BackEnd/WingBuy/src/main/java/com/lti/ui/Main.java
+++ BackEnd/WingBuy/src/main/java/com/lti/ui/Main.java
@@ -6,49 +6,137 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import com.lti.CSVReader;
 import com.lti.model.Admin;
 import com.lti.model.Order;
 import com.lti.model.Payment;
 import com.lti.model.ProductForApproval;
 import com.lti.model.Retailer;
 import com.lti.model.User;
+
+import java.util.List;
+
 import com.lti.model.Product;
-
 public class Main {
-
 	public static void main(String[] args) {
 		EntityManagerFactory factory= Persistence.createEntityManagerFactory("JPA-PU");
 		EntityManager entityManager= factory.createEntityManager();
-
 		Admin admin1 = new Admin();
-		admin1.setaName("admin1");
-        admin1.setaEmail("abc@gmail.com");
-        admin1.setaPassword("123456");
-	
-       
+		admin1.setaName("Vijay Dhakad");
+		admin1.setaEmail("vijay.dhakad@gmail.com");
+		admin1.setaPassword("v123456d");
+		
+	Admin admin2 = new Admin();
+		admin2.setaName("Gnapika Ankam");
+		admin2.setaEmail("gnapika.ankam@gmail.com");
+	      	admin2.setaPassword("g123456a");
+	      	
+	      	User user1 = new User();
+	    	user1.setuName("Arijeet B");
+	    	user1.setuEmail("arijeet.b@gmail.com");
+	    	user1.setuPassword("a123456b");
+	    	user1.setuMobile(9874563210L);
+
+	    User user2 = new User();
+	    	user2.setuName("Ridhi A");
+	    	user2.setuEmail("ridhi.a@gmail.com");
+	    	user2.setuPassword("r123456a");
+	    	user2.setuMobile(78902134566L);
+	    	
+	    User user3 = new User();
+	    	user3.setuName("Mukul K");
+	    	user3.setuEmail("mukul.k@gmail.com");
+	    	user3.setuPassword("m123456k");
+	    	user3.setuMobile(78901234554L);
+	    	
+	   /* User user4 = new User();
+	    	user1.setuName("Deepak K");
+	    	user1.setuEmail("deepak.k@gmail.com");
+	    	user1.setuPassword("d123456k");
+	    	//user1.setuMobile(7870123456);
+	    	
+	    User user5 = new User();
+	    	user1.setuName("Roja K");
+	    	user1.setuEmail("roja.k@gmail.com");
+	    	user1.setuPassword("r123456k");
+	    	//user1.setuMobile(889012345);
+	    	
+	    User user6 = new User();
+	    	user1.setuName("Abhinav C");
+	    	user1.setuEmail("abhinav.c@gmail.com");
+	    	user1.setuPassword("a123456c");
+	    	//user1.setuMobile(789912375);
+	    User user7 = new User();
+	    	user1.setuName("Arpan B");
+	    	user1.setuEmail("arpan.b@gmail.com");
+	    	user1.setuPassword("a123456b");
+	    	//user1.setuMobile(989012345);
+
+	    User user8 = new User();
+	    	user1.setuName("Indranil M");
+	    	user1.setuEmail("indranil.m@gmail.com");
+	    	user1.setuPassword("i123456m");
+	    	//user1.setuMobile(789021345);
+	    	
+	    User user9 = new User();
+	    	user1.setuName("Saptrishi C");
+	    	user1.setuEmail("saptrishi.c@gmail.com");
+	    	user1.setuPassword("s123456c");
+	    	//user1.setuMobile(789992395);
+	    	
+	    User user10 = new User();
+	    	user1.setuName("Makarand B");
+	    	user1.setuEmail("makarand.b@gmail.com");
+	    	user1.setuPassword("m123456b");
+	    	//user1.setuMobile(888012342);
+	    	
+	    User user11 = new User();
+	    	user1.setuName("Divyue S");
+	    	user1.setuEmail("divyue.s@gmail.com");
+	    	user1.setuPassword("d123456s");
+	    	//user1.setuMobile(889012377);
+	    	
+	    User user12 = new User();
+	    	user1.setuName("Nitin P");
+	    	user1.setuEmail("nitin.p@gmail.com");
+	    	user1.setuPassword("n123456p");
+	    	//user1.setuMobile(895612375);
+	    	*/
+	    	Retailer retailer1=new Retailer();
+	    	retailer1.setrName("Vishal Kumar");
+	    	retailer1.setrEmail("vishal.kumar@gmail.com");
+	    	retailer1.setrPassword("va123456k");
+	    	retailer1.setrMobile(7890852450L);
+
+	    Retailer retailer2=new Retailer();
+	    	retailer2.setrName("Pinto Bhagat");
+	    	retailer2.setrEmail("pinto.bhagat@gmail.com");
+	    	retailer2.setrPassword("p123456b");
+	    	retailer2.setrMobile(7890217536L);
+	    	 entityManager.getTransaction().begin();
+		        entityManager.persist(admin1);
+		        entityManager.persist(admin2);
+		        entityManager.persist(user1);
+		        entityManager.persist(user2);
+		        entityManager.persist(user3);
+		        entityManager.persist(retailer1);
+		        entityManager.persist(retailer2);
+		        entityManager.getTransaction().commit();
+	    	CSVReader csvreader = new CSVReader();
+	    	
+	    		List<ProductForApproval> list=csvreader.load(admin1, admin2, retailer1, retailer2);
+	    		for(ProductForApproval p :list){
+	    			 entityManager.getTransaction().begin();
+	    		        entityManager.persist(p);
+	    		        entityManager.getTransaction().commit();	
+	    		}
+
         
-        User user1 = new User();
-		user1.setuName("admin1");
-        user1.setuEmail("abc@gmail.com");
-       user1.setuPassword("123456");
-       user1.setuMobile(7896931);
-       
-    	Admin admin2 = new Admin();        
-    	admin2.setaName("admin2");
-        admin2.setaEmail("def@gmail.com");
-        admin2.setaPassword("654321");
-	
-        Retailer retailer1=new Retailer();
-        retailer1.setrName("Mobile pvt ltd");
-        retailer1.setrEmail("aaa@gmail.com");
-        retailer1.setrMobile(78953421);
-        retailer1.setrPassword("14563");
-        
         
         
        
        
-        entityManager.getTransaction().begin();
+       /* entityManager.getTransaction().begin();
         entityManager.persist(admin2);
         entityManager.getTransaction().commit();
         
@@ -65,8 +153,9 @@ public class Main {
 	    pfa.setRetailer(retailer1);
 	    pfa.setpStatus('p');
 	    pfa.setpStock(2000);
-	    
-	    
+
+
+
         ProductForApproval pfa1= new ProductForApproval();
 	    pfa1.setAdmin(admin1);
 	    pfa1.setpBrand("MOTO");
@@ -79,9 +168,9 @@ public class Main {
 	    pfa1.setRetailer(retailer1);
 	    pfa1.setpStatus('p');
 	    pfa1.setpStock(2000);
-	    
-	    
-	    
+
+
+
 	    entityManager.getTransaction().begin();
         entityManager.persist(admin1);
         entityManager.persist(pfa);
@@ -101,7 +190,8 @@ public class Main {
 	    product.setpBrand(pfa.getpBrand());
 	    product.setpStock(pfa.getpStock());
 	    product.setpImage(pfa.getpImage());
-	    
+
+
 	    Product product1= new Product();
 	    product1.setAdmin(admin1);
 	    product1.setRetailer(pfa1.getRetailer().getrId());
@@ -113,23 +203,25 @@ public class Main {
 	    product1.setpBrand(pfa1.getpBrand());
 	    product1.setpStock(pfa1.getpStock());
 	    product1.setpImage(pfa1.getpImage());
-        
+
         entityManager.getTransaction().begin();
-        ProductForApproval pfa2=entityManager.find(ProductForApproval.class,pfa.getpReqId());
-        ProductForApproval pfa3=entityManager.find(ProductForApproval.class,pfa1.getpReqId());
-        entityManager.remove(pfa2);
-        entityManager.remove(pfa3);
+        ProductForApproval rpfa=entityManager.find(ProductForApproval.class,pfa.getpReqId());
+        entityManager.remove(rpfa);
+        ProductForApproval rpfa1=entityManager.find(ProductForApproval.class,pfa1.getpReqId());
+        entityManager.remove(rpfa1);
+      
         entityManager.persist(product1);
         entityManager.persist(product);
         entityManager.getTransaction().commit();
+
 	    user1.setcProducts(new HashSet<Product>());
-        
+
         user1.addProductToCart(product1);
         user1.addProductToCart(product);
         entityManager.getTransaction().begin();
         entityManager.persist(user1);
         entityManager.getTransaction().commit();
-        
+
         Payment payment1=new Payment();
         payment1.setPayType("COD");
         user1.setPayments(new HashSet<Payment>());
@@ -141,16 +233,12 @@ public class Main {
         order1.setPayment(payment1);
         order1.setUser(user1);
         order1.setpOrderDetails(new HashSet<Product>());
-        order1.addProductToOrder(product1);
+        order1.addProductToOrderDetails(product1);
         order1.setoAddress("mahape");
         entityManager.getTransaction().begin();
-        
+
         entityManager.persist(order1);
         entityManager.getTransaction().commit();
-	    
+*/
 	}
-	
-    
-     
 }
-//hello
